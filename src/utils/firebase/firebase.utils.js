8a8f2038cc6ec277a80f2dfdd667ddfb7640f8dc
Nivesh Syreddy <niveshsyreddy@gmail.com>
2022-10-25 13:32:30 +0530

    create sign-up component, button

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -4,6 +4,7 @@ import {
   signInWithRedirect,
   signInWithPopup,
   GoogleAuthProvider,
+  createUserWithEmailAndPassword
 } from "firebase/auth";
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
@@ -19,17 +20,20 @@ const firebaseConfig = {
 
 const firebaseapp = initializeApp(firebaseConfig);
 
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
     prompt: "select_account"
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopup = () => signInWithPopup (auth, provider);
+export const signInWithGooglePopup = () => signInWithPopup (auth, googleProvider);
+export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
 export const db = getFirestore();
 
-export const createuserDocumentFromAuth = async (userAuth) => {
+export const createuserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
+    if(!userAuth) return ;
+
     const userDocRef = doc(db, 'users', userAuth.uid);
     console.log(userDocRef);
 
@@ -44,7 +48,8 @@ export const createuserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation
             });
         } catch (error) {
             console.log('error creating the user', error.message);
@@ -57,4 +62,10 @@ export const createuserDocumentFromAuth = async (userAuth) => {
 
     //if user data exists
     //return the user
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return; 
+
+    return await createUserWithEmailAndPassword(auth, email, password);
 };
