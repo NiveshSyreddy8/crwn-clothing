55b953dab600e68df9bfe468e23e545cc640ea8c
Nivesh Syreddy <niveshsyreddy@gmail.com>
2022-10-26 21:54:02 +0530

    Add Ckeckout page anf functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../context/cart.context";
 import Button from "../button/button.component";
 import "./cart-dropdown.styles.scss";
@@ -6,6 +7,11 @@ import CartItem from "../cart-item/cart-item.component";
 
 const CartDropDown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('./checkout')
+  }
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
@@ -13,7 +19,7 @@ const CartDropDown = () => {
           <CartItem key={item.id} CartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT </Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT </Button>
     </div>
   );
 };
